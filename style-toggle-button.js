ccf735b19f18542c0436d74800991df4b9368d95
Son Le Si <sonpro.10c6@gmail.com>
2019-08-29 23:31:26 +0700

    Create style-toggle-button.js

---
+++ style-toggle-button.js
@@ -0,0 +1,14 @@
+
+  viewToggle: {
+    height: 22,
+    width: 45,
+    borderRadius: 20,
+    justifyContent: 'center'
+  },
+  circleToggle: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: '#fff'
+  }
+ 
